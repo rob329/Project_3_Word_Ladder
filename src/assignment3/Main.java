fa4d3b19f877e2dc3db3787afee84b82e42a919f
akhilmujje <akhil.mujje@utexas.edu>
2016-09-27 19:16:58 -0500

    update tree
    checking through dictinary

--- src/assignment3/Main.java
+++ src/assignment3/Main.java
@@ -12,78 +12,108 @@
  * Fall 2016
  */
 
-
 package assignment3;
+
 import java.util.*;
 import java.io.*;
 
 public class Main {
-	
+
 	// static variables and constants only here.
-	
+	public static final String[] alphabet = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O",
+			"P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+	public static final int word_length = 5;
+
 	public static void main(String[] args) throws Exception {
-		
-		Scanner kb;	// input Scanner for commands
-		PrintStream ps;	// output file
-		// If arguments are specified, read/write from/to files instead of Std IO.
+
+		Scanner kb; // input Scanner for commands
+		PrintStream ps; // output file
+		// If arguments are specified, read/write from/to files instead of Std
+		// IO.
 		if (args.length != 0) {
 			kb = new Scanner(new File(args[0]));
 			ps = new PrintStream(new File(args[1]));
-			System.setOut(ps);			// redirect output to ps
+			System.setOut(ps); // redirect output to ps
 		} else {
 			kb = new Scanner(System.in);// default from Stdin
-			ps = System.out;			// default to Stdout
+			ps = System.out; // default to Stdout
 		}
-		getWordLadderBFS("yes" , " no");
 		initialize();
-		
+
 		// TODO methods to read in words, output ladder
+
+		getWordLadderBFS("stone", "sad");
 	}
-	
+
 	public static void initialize() {
 		// initialize your static variables or constants here.
-		// We will call this method before running our JUNIT tests.  So call it 
+		// We will call this method before running our JUNIT tests. So call it
 		// only once at the start of main.
+
 	}
-	
+
 	/**
-	 * @param keyboard Scanner connected to System.in
-	 * @return ArrayList of 2 Strings containing start word and end word. 
-	 * If command is /quit, return empty ArrayList. 
+	 * @param keyboard
+	 *            Scanner connected to System.in
+	 * @return ArrayList of 2 Strings containing start word and end word. If
+	 *         command is /quit, return empty ArrayList.
 	 */
 	public static ArrayList<String> parse(Scanner keyboard) {
 		// TO DO
 		return null;
 	}
-	
+
 	public static ArrayList<String> getWordLadderDFS(String start, String end) {
-		
-		// Returned list should be ordered start to end.  Include start and end.
+
+		// Returned list should be ordered start to end. Include start and end.
 		// Return empty list if no ladder.
 		// TODO some code
-		Set<String> dict  = makeDictionary();
+		Set<String> dict = makeDictionary();
 		// TODO more code
-		
+
 		return null; // replace this line later with real return
 	}
-	
-    public static ArrayList<String> getWordLadderBFS(String start, String end) {
-		
+
+	public static ArrayList<String> getWordLadderBFS(String start, String end) {
+		start = start.toUpperCase();
 		// TODO some code
 		Set<String> dict = makeDictionary();
-		if(dict.contains("ABAYA")){
-			System.out.println("Yes");
-		}
-		// TODO more code
+
+		Node<String> startNode = new Node<String>(start);
 		
+		
+		String check = "";
+		for (int i = 0; i < word_length; i++)
+			for (int j = 0; j < alphabet.length; j++) {
+	//			if (start.charAt(i) != alphabet[j].charAt(0)){
+					check = start.substring(0, i) + alphabet[j].charAt(0)+ start.substring(i+1);
+					if(dict.contains(check)){
+					 startNode.addChild(check, startNode);					 
+					}
+				
+				
+
+			}
+
+		
+		Node<String> childNode2 = new Node<String>("Child 2");
+		childNode2.setParent(startNode);
+
+		if (dict.contains("AGONY")) {
+			System.out.println("agony");
+
+		}
+
+		// TODO more code
+
 		return null; // replace this line later with real return
 	}
-    
-	public static Set<String>  makeDictionary () {
+
+	public static Set<String> makeDictionary() {
 		Set<String> words = new HashSet<String>();
 		Scanner infile = null;
 		try {
-			infile = new Scanner (new File("five_letter_words.txt"));
+			infile = new Scanner(new File("five_letter_words.txt"));
 		} catch (FileNotFoundException e) {
 			System.out.println("Dictionary File not Found!");
 			e.printStackTrace();
@@ -94,11 +124,10 @@ public class Main {
 		}
 		return words;
 	}
-	
+
 	public static void printLadder(ArrayList<String> ladder) {
-		
+
 	}
 	// TODO
 	// Other private static methods here
 }
-
---
+++ src/assignment3/Node.java
@@ -0,0 +1,54 @@
+package assignment3;
+
+import java.util.ArrayList;
+
+public class Node<String> {
+
+	private ArrayList<Node<String>> children = new ArrayList<Node<String>>();
+	private Node<String> parent = null;
+	private String word = null;
+	
+	public Node(String word){
+		this.word = word;
+	}
+
+	public Node(String word, Node<String> parent){
+		this.word = word;
+		this.parent = parent;
+	}
+	
+	
+	public ArrayList<Node<String>> getChildren(){
+		return children;
+	}
+	
+	public void setParent(Node<String> parent){
+		parent.addChild(this);
+		this.parent = parent;
+	}
+	
+	public void addChild(String word, Node<String> parent){
+		Node<String> child = new Node<String>(word);
+		child.setParent(parent);
+		this.children.add(child);
+	}
+	
+	public void addChild(Node<String> child) {
+        //child.setParent(this);
+        this.children.add(child);
+    }
+	
+	public String getWord(){
+		return this.word;
+	}
+	
+	public void setWord(String word){
+		this.word = word;
+	}
+	
+	
+	
+	
+	
+	
+}
